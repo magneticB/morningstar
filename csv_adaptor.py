60ec0a0bf285344df881912d7017cbff39a5d1e1
Tom Townsend <ttownsend@deloitte.com>
2021-01-30 19:52:38 -0800

    Morningstar UK screenscraper

---
+++ csv_adaptor.py
@@ -0,0 +1,55 @@
+import requests
+from lxml import html
+import re
+
+from pandas.core import index
+
+from morningstar import Morningstar
+import pandas as pd
+import csv
+
+
+FILENAME = "Investments - Sheet1"
+
+class CSVAdaptor:
+
+    def read_csv_and_update(self):
+        ms = Morningstar()
+
+        df = pd.read_csv(FILENAME+".csv", header=0)
+
+        df = df.apply(self.lookup_morningstar, ms=ms, axis=1)
+
+        df.to_csv(FILENAME+" - Output.csv", index=False)
+
+    def lookup_morningstar(self, row, ms):
+
+        if (row['Exchange'] == 'LSE'):  # only UK for now
+            ticker = row['Ticker']
+            override = row['Override Ticker']
+
+            if not pd.isnull(override):
+                ticker = override
+
+            ms_id = ms.get_morningstar_id_by_ticker(ticker)
+            ms_overview = ms.get_overview_by_id(ms_id)
+            ms_performance = ms.get_performance_by_id(ms_id)
+
+            row['URL'] = ms.get_external_url(ms_id)
+            row['Name'] = ms_overview['Name']
+            row['Charge'] = ms_overview['Charge']
+            row['Morningstar Rating'] = ms_overview['Stars']
+            row['Category'] = ms_overview['Category']
+            row['Price'] = ms_overview['Price']
+            row['3 Months'] = ms_performance['3 Months']
+            row['6 Months'] = ms_performance['6 Months']
+            row['1 Year'] = ms_performance['1 Year']
+            row['3 Years'] = ms_performance['3 Years Annualised']
+            row['5 Years'] = ms_performance['5 Years Annualised']
+            row['10 Years'] = ms_performance['10 Years Annualised']
+        return row
+    
+if __name__ == '__main__':
+    a = CSVAdaptor()
+    a.read_csv_and_update()
+
